f1f96196ed9008269928dd07b8084260f1c58641
ZawadiSumba66 <rosesumba98@gmail.com>
2021-04-06 13:43:05 +0300

    cleaning up dist folder

--- src/index.js
+++ src/index.js
@@ -1,33 +1,15 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './icon.png'
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
-  
+    const btn = document.createElement('button');
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello')
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    
-     element.appendChild(myIcon);
-     console.log(Data);
-  console.log(Notes);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,19 @@
 const path = require('path');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Output Management',
+        }),
+      ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
+    clean: true,
   },
 };
